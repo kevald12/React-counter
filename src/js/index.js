773641e8eaa8c18ed7f23d1d253daf040e0630fe
kevald12 <kealable@gmail.com>
2022-09-10 03:02:14 +0000

    finished my project with react

--- src/js/index.js
+++ src/js/index.js
@@ -5,8 +5,42 @@ import ReactDOM from "react-dom";
 // include your styles into the webpack bundle
 import "../styles/index.css";
 
-//import your own components
-import Home from "./component/home.jsx";
+//import proptypes
+import PropTypes from 'prop-types';
+
+//creating the component
+const SimpleCounter = (props) =>{
+    return (<div className="bigCounter">
+        <div className="calendar">
+        <i className="fas fa-clock"></i>
+        </div>
+        <div className="four">{props.n4 % 10 }</div>
+        <div className="three">{props.n3 % 10}</div>
+        <div className="two">{props.n2 % 10}</div>
+        <div className="one">{props.n1 % 10}</div>
+    </div>);
+}
+
+SimpleCounter.prototype = {
+n1:PropTypes.number, 
+n2:PropTypes.number,
+n3:PropTypes.number,
+n4:PropTypes.number,
+};
+
+
+let counter = 0;
+setInterval(() => {
+    const four = Math.floor(counter/1000);
+    const three = Math.floor(counter/100);
+    const two = Math.floor(counter/10);
+    const one = Math.floor(counter/1);
+    console.log(four,three,two,one);
+
+    counter++;
+
+
 
 //render your react application
-ReactDOM.render(<Home />, document.querySelector("#app"));
+ReactDOM.render(<SimpleCounter n1 = {one} n2 = {two} n3 = {three} n4 = {four}/>, document.querySelector("#app"));
+}, 100);
